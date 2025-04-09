70c591483a760542c5902ecf45d1e798f73375c5
zh-programmer <zhprogrammer01@gmail.com>
2025-04-09 10:49:34 -0700

    Auto-generated commit

--- main-RGZ4FAAD.js
+++ main-RGZ4FAAD.js
@@ -119,7 +119,7 @@ end`};e.events.push(["putFont",function(s){(function(o){var a=o.font,c=o.out,u=o
 
 `),this.createCommandBuffer([27,97,0]),Fs(this.formatLine("Product      ","Cat      ","Qty     ","Price     ","Total     ",r)+`
 `),Fs(`------------------------------
-`)];e.forEach(s=>{let o=this.formatLine(this.truncate(s.productName,14),this.truncate(s.categoryName,14),s.quantity.toString().padStart(5),`$${s.price.toFixed(2)}`.padStart(10),`$${(s.price*s.quantity).toFixed(10)}`,r);i.push(Fs(o+`
+`)];e.forEach(s=>{let o=this.formatLine(this.truncate(s.productName,14),this.truncate(s.categoryName,13),s.quantity.toString().padStart(4),`$${s.price.toFixed(2)}`.padStart(9),`$${(s.price*s.quantity).toFixed(9)}`,r);i.push(Fs(o+`
 `))}),i.push(Fs(`
 `),Fs(this.rightAlign(`Subtotal: $${t.toFixed(2)}`,r)+`
 `),Fs(this.rightAlign("Discount (0%): $0.00",r)+`
