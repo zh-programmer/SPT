c552bd9e971b7248e292d8abc666d3b76121672b
zh-programmer <zhprogrammer01@gmail.com>
2025-04-10 10:52:15 -0700

    Auto-generated commit

--- main-6QMWVOW4.js
+++ main-6QMWVOW4.js
@@ -117,7 +117,7 @@ end`};e.events.push(["putFont",function(s){(function(o){var a=o.font,c=o.out,u=o
 `),this.createCommandBuffer([27,97,1]),as(`M&R MOBILES
 `),as(`_________
 
-`),this.createCommandBuffer([27,97,0]),as(this.formatHeader("Product      ","           ","Qty      ","Price   ","Total    ",r)+`
+`),this.createCommandBuffer([27,97,0]),as(this.formatHeader("Product      ","          ","Qty      ","Price   ","Total    ",r)+`
 `),as(`-----------------------------------------------------
 `)];e.forEach(s=>{let o=this.formatLine(s.productName,s.categoryName,s.quantity,s.price,s.price*s.quantity,r);i.push(as(o+`
 `))}),i.push(as(`
