10728cfdd9ce61b9e31a2321581efd657f8acf58
zh-programmer <zhprogrammer01@gmail.com>
2025-04-10 12:25:47 -0700

    Auto-generated commit

--- main-L3C5D527.js
+++ main-L3C5D527.js
@@ -113,7 +113,8 @@ end`};e.events.push(["putFont",function(s){(function(o){var a=o.font,c=o.out,u=o
 `),_r(`Gampaha
 `),_r(`0775815415 / 0753820515 / 0712815415
 
-`),_r(`Date: ${new Date().toLocaleDateString()}`),_r(`Time: ${new Date().toLocaleTimeString()}`),this.createCommandBuffer([27,97,0]),_r(`------------------------------------------------
+`),_r(`Date: ${new Date().toLocaleDateString()}     `),_r(`Time: ${new Date().toLocaleTimeString()}
+`),this.createCommandBuffer([27,97,0]),_r(`------------------------------------------------
 `),_r(this.formatHeader("Product      ","          ","Qty    ","Price   ","Total    ",r)+`
 `),_r(`------------------------------------------------
 `)];e.forEach(s=>{let o=this.formatLine(s.productName,s.categoryName,s.quantity,s.price,s.price*s.quantity,r);i.push(_r(o+`
